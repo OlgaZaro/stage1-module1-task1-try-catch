ba2b2caff4bbe31d962528a99b422f9831e93a2d
OlgaZaro <93153794+OlgaZaro@users.noreply.github.com>
2022-10-06 15:02:35 +0300

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -10,13 +10,10 @@ import java.lang.NumberFormatException;
  * space delimiter
  */
 public class ParseIntegers {
-
-
- private static final List<String> WORDS =
+   private static final List<String> WORDS =
             Arrays.asList(
                     "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
-
     public static void main(String[] args) throws NumberFormatException {
         Iterator<String> words = WORDS.iterator();
         int sum = 0;
