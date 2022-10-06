a4082326a9ab2b17bc3279a1cfc34986cd60e651
OlgaZaro <93153794+OlgaZaro@users.noreply.github.com>
2022-10-06 14:44:36 +0300

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -3,6 +3,7 @@ package com.epam.m1.exceptions;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.lang.NumberFormatException;
 
 /**
  * Complete the code, parse integers, calculate the sum of numbers in the WORDS, join strings with
@@ -11,7 +12,7 @@ import java.util.List;
 public class ParseIntegers {
 
 
- private static final List<String> WORDS =
+  private static final List<String> WORDS =
             Arrays.asList(
                     "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
@@ -29,8 +30,10 @@ public class ParseIntegers {
                 sum += number;
 
             } catch (NumberFormatException e) {
+                if (!builder.isEmpty()) {
+                    builder.append(" ");
+                }
                 builder.append(next);
-                builder.append(" ");
                 justWords = builder.toString();
             }
         }
