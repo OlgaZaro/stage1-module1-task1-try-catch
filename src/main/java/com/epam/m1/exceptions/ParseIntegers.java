2fa972a647540c599b6213b33f1cbef763e452b1
OlgaZaro <93153794+OlgaZaro@users.noreply.github.com>
2022-10-06 14:59:54 +0300

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -12,9 +12,8 @@ import java.lang.NumberFormatException;
 public class ParseIntegers {
 
 
- static final List<String> WORDS =
-            Arrays.asList(
-                    "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
+ private static final List<String> WORDS =
+            Arrays.asList("JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
 
     public static void main(String[] args) throws NumberFormatException {
