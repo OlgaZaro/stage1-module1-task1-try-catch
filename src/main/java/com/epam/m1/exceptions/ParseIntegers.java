de2804a965da805abe24a3a5dff910c0bf319cce
OlgaZaro <93153794+OlgaZaro@users.noreply.github.com>
2022-10-06 14:50:59 +0300

    Update ParseIntegers.java

--- src/main/java/com/epam/m1/exceptions/ParseIntegers.java
+++ src/main/java/com/epam/m1/exceptions/ParseIntegers.java
@@ -12,7 +12,7 @@ import java.lang.NumberFormatException;
 public class ParseIntegers {
 
 
- private static final List<String> WORDS =
+ public static final List<String> WORDS =
             Arrays.asList(
                     "JDK 17 has released on 14 September 2021 with 10 new features, 2 feature removals and 2 feature deprecations."
                             .split(" "));
